94a03ca39b864b89a5dcfec5565697da15397eca
testertester193 <testertester193@gmail.com>
2020-11-15 16:46:28 +0000

    nsnnsnsns

---
+++ init.R
@@ -0,0 +1,11 @@
+my_packages = c("plyr","shiny","shinythemes","leaflet","dplyr","markovifyR")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
