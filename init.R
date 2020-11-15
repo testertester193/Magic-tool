699301b48a06f9662ddb903e60ef57ffc123eb94
testertester193 <testertester193@gmail.com>
2020-11-15 17:52:52 +0000

    ejkdd,jfbdxjdfhjkfbfjdufhdf

--- init.R
+++ init.R
@@ -10,5 +10,5 @@ install_if_missing = function(p) {
 }
 invisible(sapply(my_packages, install_if_missing))
 
-devtools::install_github('abresler/markovifyR')
+devtools::install_github('abresler/markovifyR', force = TRUE)
 markovifyR::install_markovify()
